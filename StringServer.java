ea67fa01d5db299414a6ba0cc59a38d8e78d21d5
estherxiong <ejxiong@ucsd.edu>
2023-01-29 00:48:06 -0800

    Create StringServer.java

---
+++ StringServer.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+
+    String output = "";
+    public String handleRequest(URI url) {
+        System.out.println("Path: " + url.getPath());
+        if (url.getPath().contains("/add-message")) {
+            String[] parameters = url.getQuery().split("=");
+            output+=parameters[1] + '\n';
+        }
+        return output;
+    }
+}
+
+class StringServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
